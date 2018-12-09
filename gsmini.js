a9dd3322d9c6853cb81fe5c3356548340031fe27
Saith Kay <kaysaith@sina.com>
2018-12-09 19:41:10 +0800

    [Feature] get language code

--- gsmini.js
+++ gsmini.js
@@ -1,33 +1,13 @@
-function chainType (n) {return typeof n === Tag.Int ? n : void 0}
-
-function toastMessage (n) {window.control.toastMessage(n)}
-
-function showAlert (n, a, e) {
-  if (n === CustomizedCommand.ConfirmEvent) return confirmEventCallback(), void(confirmEventCallback = null)
-  typeof a !== Tag.String || typeof e !== Tag.String || typeof n !== Tag.Function ? alert('Wrong Parameters of Requesting') : (confirmEventCallback = n, window.control.alert(a, e))
-}
-
-function getChainID (n, a) {void 0 !== a && void 0 === chainType(a) ? n('wrong chain type') : typeof n === Tag.Function ? (getChainIDCallback = n, window.control.getChainID(a)) : typeof n === Tag.String ? (getChainIDCallback(n), getChainIDCallback = null) : (getChainIDCallback('wrong request'), getChainIDCallback = null)}
-
-function getAccountAddress (n, a, e, t) {void 0 !== a && void 0 !== e && typeof t === Tag.Boolean || typeof n === Tag.String ? typeof n === Tag.Function ? (getAccountAddressCallback = n, window.control.getAccountAddress(a, e, t)) : typeof n === Tag.String ? (getAccountAddressCallback(n), getAccountAddressCallback = null) : (getAccountAddressCallback('wrong request'), getAccountAddressCallback = null) : n('wrong parameters of requesting')}
-
-function getBalance (n, a, e) {void 0 !== a && void 0 !== e || typeof n === Tag.String ? typeof n === Tag.Function ? (getBalanceCallback = n, window.control.getBalance(a, e)) : typeof n === Tag.String ? (getBalanceCallback(decodeURIComponent(n)), getBalanceCallback = null) : (getBalanceCallback('wrong request'), getBalanceCallback = null) : n('wrong parameters of requesting')}
-
-function encrypt (n, a) {typeof n === Tag.Function ? typeof a === Tag.String ? (encryptCallback = n, window.control.encrypt(a)) : n('wrong request message') : typeof n === Tag.String ? (encryptCallback(decodeURIComponent(n)), encryptCallback = null) : (encryptCallback('wrong request'), encryptCallback = null)}
-
-function decrypt (n, a) {typeof n === Tag.Function ? typeof a === Tag.String ? (decryptCallback = n, window.control.decrypt(a)) : n('wrong request message') : typeof n === Tag.String ? (decryptCallback(n), decryptCallback = null) : (decryptCallback('wrong request'), decryptCallback = null)}
-
-function getSignHeader (n) {typeof n === Tag.Function && (signHeaderCallback = n, window.control.getSignHeader()), typeof n === Tag.String && (signHeaderCallback(n), signHeaderCallback = null)}
-
-function getEOSSignedData (n, a) {
-  if (typeof n === Tag.Function && (typeof a === Tag.String ? (signedDataCallback = n, window.control.getEOSSingedData(a)) : n('wrong request message')), typeof n === Tag.String) {
-    var e = decodeURIComponent(n)
-    signedDataCallback(e), signedDataCallback = null
+eval(function(d, e, a, c, b, f) {
+  b = function(a) {return (a < e ? '' : b(parseInt(a / e))) + (35 < (a %= e) ? String.fromCharCode(a + 29) : a.toString(36))}
+  if (!''.replace(/^/, String)) {
+    for (; a--;) f[b(a)] = c[a] || b(a)
+    c = [function(a) {return f[a]}]
+    b = function() {return '\\w+'}
+    a = 1
   }
-}
-
-var Tag = { Function: 'function', String: 'string', Int: 'number', Boolean: 'boolean' },
-  ChainType = { EOS: chainType(194), BTC: chainType(0), LTC: chainType(2) },
-  CustomizedCommand = { FinishedBackEvent: 'finished', ConfirmEvent: 'clickedConfirmButton' }, confirmEventCallback,
-  getChainIDCallback, getAccountAddressCallback, getBalanceCallback, encryptCallback, decryptCallback,
-  signHeaderCallback, signedDataCallback, backEvent
+  for (; a--;) c[a] && (d = d.replace(new RegExp('\\b' + b(a) + '\\b', 'g'), c[a]))
+  return d
+}('8 1={g:\'e\',6:\'10\',y:\'V\',G:\'S\'}8 X={O:q(U),Y:q(0),11:q(2)}8 L={P:\'Q\',J:\'T\'}e q(a){4(3 a===1.y)u a 5 u j}e D(a){f.h.D(a)}8 t e R(a,b,c){4(a===L.J){t()t=7 u}4((3 b!==1.6||3 c!==1.6||3 a!==1.g)){A(\'W N v Z\')}5{t=a f.h.A(b,c)}}8 p e I(a,b){4(b!==j&&q(b)===j){a(\'9 12 13\')}5{4(3 a===1.g){p=a f.h.I(b)}5 4(3 a===1.6){p(a)p=7}5{p(\'9 i\')p=7}}}8 k e F(a){4(3 a===1.g){k=a f.h.F()}5 4(3 a===1.6){k(a)k=7}5{k(\'9 i\')k=7}}8 m e z(a,b,c,d){4((b===j||c===j||3 d!==1.G)&&3 a!==1.6){a(\'9 B v C\')}5{4(3 a===1.g){m=a f.h.z(b,c,d)}5 4(3 a===1.6){m(a)m=7}5{m(\'9 i\')m=7}}}8 n e E(a,b,c){4((b===j||c===j)&&3 a!==1.6){a(\'9 B v C\')}5{4(3 a===1.g){n=a f.h.E(b,c)}5 4(3 a===1.6){n(w(a))n=7}5{n(\'9 i\')n=7}}}8 o e H(a,b){4(3 a===1.g){4(3 b===1.6){o=a f.h.H(b)}5 a(\'9 i x\')}5 4(3 a===1.6){o(w(a))o=7}5{o(\'9 i\')o=7}}8 l e K(a,b){4(3 a===1.g){4(3 b===1.6){l=a f.h.K(b)}5 a(\'9 i x\')}5 4(3 a===1.6){l(a)l=7}5{l(\'9 i\')l=7}}8 s e M(a){4(3 a===1.g){s=a f.h.M()}4(3 a===1.6){s(a)s=7}}8 r e 14(a,b){4(3 a===1.g){4(3 b===1.6){r=a f.h.15(b)}5 a(\'9 i x\')}4(3 a===1.6){8 c=w(a)r(c)r=7}}8 16',
+  62, 69, ' Tag  typeof if else String null var wrong     function window Function control request undefined getLanguageCodeCallback decryptCallback getAccountAddressCallback getBalanceCallback encryptCallback getChainIDCallback chainType signedDataCallback signHeaderCallback confirmEventCallback return of decodeURIComponent message Int getAccountAddress alert parameters requesting toastMessage getBalance getLanguageCode Boolean encrypt getChainID ConfirmEvent decrypt CustomizedCommand getSignHeader Parameters EOS FinishedBackEvent finished showAlert boolean clickedConfirmButton 194 number Wrong ChainType BTC Requesting string LTC chain type getEOSSignedData getEOSSingedData backEvent'.split(' '),
+  0, {}))
